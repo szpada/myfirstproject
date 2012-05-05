7ce4321397de8ad5af432631ad6310c96843a86b
Maciej Pajak <pajak.m@gmail.com>
2012-05-05 15:39:57 +0100

    kolejne

--- src/com/edu4java/android/killthemall/EnemyAttack.java
+++ src/com/edu4java/android/killthemall/EnemyAttack.java
@@ -8,7 +8,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
-enum enemyAttackType{spear};
+enum enemyAttackType{spear,catapult_stone};
 
 enum attackState{fly,die};
 
