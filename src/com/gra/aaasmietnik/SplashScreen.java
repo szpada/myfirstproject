f5dc2aefa9660c0edf04b653ef351306db206d5c
Maciej Pajak <pajak.m@gmail.com>
2012-05-18 19:45:37 +0100

    splasz

---
+++ src/com/gra/aaasmietnik/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.gra.aaasmietnik;
+
+import android.app.Activity;
+
+
+
+/**
+ * @author Maciej
+ * Tu bedzie nasz 'reklamowy' splash screen przy uruchamianiu gry (i moze przy ladowaniu...)
+ */
+public class SplashScreen extends Activity {
+
+}
