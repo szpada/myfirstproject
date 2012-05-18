a995731c0e5b2dbb2a2da797c13aa28581e9788e
Maciej Pajak <pajak.m@gmail.com>
2012-05-18 19:47:33 +0100

    smieci

--- src/com/gra/aaasmietnik/Help.java
+++ src/com/gra/aaasmietnik/Help.java
@@ -1,16 +1,16 @@
-package com.gra.menu;
+package com.gra.aaasmietnik;
 
 import com.gra.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Credits extends Activity {
+public class Help extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.credits);      
+        setContentView(R.layout.help);      
               
     }
 }
